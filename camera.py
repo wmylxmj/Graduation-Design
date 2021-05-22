a631b86eccd03bda4ff365c05d98a085c7e7cda7
wmy <1397322329@qq.com>
2021-05-23 01:11:53 +0800

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 21 21:08:15 2021
+
+@author: wmy
+"""
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import os
+from model import SPENet, TCPNet
+import time
+
+def get_skeleton_points(heatmaps, visual_threshold=0.5):
+    skeleton = [[16, 14], [14, 12], [17, 15], [15, 13], \
+                [12, 13], [6, 12], [7, 13], [6, 7], \
+                [6, 8], [7, 9], [8, 10], [9, 11], \
+                [2, 3], [1, 2], [1, 3], [2, 4], \
+                [3, 5], [4, 6], [5, 7]]
+    visual = []
+    for line in skeleton:
+        channel1 = line[0] - 1
+        channel2 = line[1] - 1
+        heatmap1 = heatmaps[:, :, channel1]
+        heatmap2 = heatmaps[:, :, channel2]
+        point1 = np.unravel_index(heatmap1.argmax(), heatmap1.shape)
+        point2 = np.unravel_index(heatmap2.argmax(), heatmap2.shape)
+        if heatmap1[point1[0]][point1[1]] < visual_threshold or heatmap2[point2[0]][point2[1]] < visual_threshold:
+            visual.append(None)
+            pass
+        else:
+            p1 = (point1[1]*2, point1[0]*2)
+            p2 = (point2[1]*2, point2[0]*2)
+            visual.append([p1, p2])
+            pass
+        pass
+    return visual
+
+def get_pose_vector(heatmaps):
+    vector = []
+    for joint in range(heatmaps.shape[-1]):
+        heatmap = heatmaps[:, :, joint]
+        point = np.unravel_index(heatmap.argmax(), heatmap.shape)
+        vector.append(point[1])
+        vector.append(point[0])
+        vector.append(heatmap[point[0]][point[1]])
+        pass
+    return vector
+        
+def hsv2rgb(h, s, v):
+    # h 0-360 s 0-1 v 0-1
+    h = h % 360
+    c = v * s
+    # x = c * (1 - |(h/60) mod 2 - 1|)
+    x = c * (1 - abs((h / 60.0) % 2 - 1))
+    m = v - c
+    if (h >= 0 and h < 60) or h==360:
+        r, g, b = c, x, 0
+        pass
+    elif h >= 60 and h < 120:
+        r, g, b = x, c, 0
+        pass
+    elif h >= 120 and h < 180:
+        r, g, b = 0, c, x
+        pass
+    elif h >= 180 and h < 240:
+        r, g, b = 0, x, c
+        pass
+    elif h >= 240 and h < 300:
+        r, g, b = x, 0, c
+        pass
+    elif h >= 300 and h < 360:
+        r, g, b = c, 0, x
+        pass
+    r, g, b = (r + m) *255, (g + m) *255, (b + m) *255
+    # to uint8
+    r = int(np.uint8(np.rint(np.maximum(np.minimum(r, 255), 0))))
+    g = int(np.uint8(np.rint(np.maximum(np.minimum(g, 255), 0))))
+    b = int(np.uint8(np.rint(np.maximum(np.minimum(b, 255), 0))))
+    return r, g, b
+
+def get_colors(num, mode="rgb"):
+    hs = [360 / num * i + 0.5 * 360 / num for i in range(num)]
+    colors = []
+    for h in hs:
+        r, g, b = hsv2rgb(h, s=1, v=1)
+        if mode == "rgb" or mode == "RGB":
+            colors.append((r, g, b))
+            pass
+        elif mode == "bgr" or mode == "BGR":
+            colors.append((b, g, r))
+            pass
+        else:
+            raise Exception("Unkown mode.") 
+            pass
+        pass
+    return colors
+
+category = {}
+category["0"] = "Wiping sweat"
+category["1"] = "Fanning with hands"
+category["2"] = "Shaking T-shirt"
+category["3"] = "Rubbing hands"
+category["4"] = "Collar tuggingt"
+category["5"] = "Contracted posture"
+category["6"] = "Undefined posture"
+
+# skeleton has 19 lines
+colors = get_colors(19, mode="BGR")
+
+# model settings
+SPENet_layers = 8
+SPENet_joints = 17
+
+SPENet_weights = "weights/SPENet-8-17.h5"
+
+TCPNet_step = 5
+TCPNet_features = 51
+TCPNet_ndim = 7
+TCPNet_units = 256
+
+TCPNet_weights = "weights/TCPNet-5-51-7-256.h5"
+
+print("CREATING SPENET...")
+spe = SPENet(layers=SPENet_layers, joints=SPENet_joints)
+print("SPENET LOADING WEIGHTS...")
+spe.load_weights(SPENet_weights)    
+print("CREATING TCPNET...")
+tcp = TCPNet(step=TCPNet_step, features=TCPNet_features, ndim=TCPNet_ndim, units=TCPNet_units)
+print("TCPNET LOADING WEIGHTS...")
+tcp.load_weights(TCPNet_weights)    
+
+sequence = []
+for i in range(TCPNet_step):
+    sequence.append([0.0 for j in range(TCPNet_features)])
+    pass
+
+heatmap_threshold = 0.5
+
+def camera_run():
+    # camera
+    camera = cv2.VideoCapture(0)
+    count = 0
+    while True:
+        see_whole_body = True
+        ret, frame = camera.read()
+        # input  BGR TO RGB
+        X = np.array(frame[...,::-1])
+        X = np.float32([X])
+        # predict heatmap
+        heatmaps = spe.predict(X)[0]
+        skeleton = get_skeleton_points(heatmaps, heatmap_threshold)
+        pose_vector = get_pose_vector(heatmaps)
+        sequence.insert(0, pose_vector)
+        sequence.remove(sequence[TCPNet_step])
+        count += 1
+        # process
+        s = np.float32(sequence)
+        w_min = np.min(s[:, 0::3])
+        w_max = np.max(s[:, 0::3])
+        h_min = np.min(s[:, 1::3])
+        h_max = np.max(s[:, 1::3])
+        # w
+        s[:, 0::3] -= w_min
+        s[:, 0::3] /= (w_max - w_min)
+        # h
+        s[:, 1::3] -= h_min
+        s[:, 1::3] /= (h_max - h_min)
+        s = np.float32([s])
+        # draw lines
+        for i, line in enumerate(skeleton):
+            if line == None:
+                see_whole_body = False
+                continue
+            cv2.line(frame, line[0], line[1], colors[i], thickness=2)
+            pass
+        if count >= 5:
+            a0 = np.zeros((1, TCPNet_units))
+            c0 = np.zeros((1, TCPNet_units))
+            y = tcp.predict([s, a0, c0])[0]
+            index = np.argmax(y)
+            cv2.putText(frame, "{}: {}".format(category[str(index)], y[index]), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            pass
+        cv2.imwrite("video/{}.jpg".format(time.time()), frame)
+        resized = cv2.resize(frame, (1920, 1080))
+        cv2.imshow("camera", resized)
+        # exit camera
+        if cv2.waitKey(100) & 0xff == ord('q'):
+            break
+        pass
+    camera.release()
+    cv2.destroyAllWindows()
+    pass
+
+camera_run()
---
+++ model.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 23 15:58:31 2021
+@author: wmy
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras import backend as K
+from keras.layers import Add, Conv2D, Input, Lambda, Activation, Conv2DTranspose
+from keras.models import Model
+from keras.layers import Conv3D, ZeroPadding3D, BatchNormalization, Multiply
+from keras.layers import LeakyReLU, concatenate, Reshape, Softmax, MaxPool2D
+from keras.layers import Dense
+from keras.layers import Conv1D, ZeroPadding1D, AveragePooling1D, Flatten
+from keras.layers import Dropout
+from IPython.display import SVG
+from keras.utils import plot_model
+from tensorflow.python import keras
+from keras.layers import LSTM, RepeatVector
+
+'''SPENet'''
+
+def SubpixelConv2D(scale, **kwargs):
+    return Lambda(lambda x: tf.depth_to_space(x, scale), **kwargs)
+
+def DesubpixelConv2D(scale, **kwargs):
+    return Lambda(lambda x: tf.space_to_depth(x, scale), **kwargs)
+
+def Normalization(**kwargs):
+    return Lambda(lambda x: x / 127.5 - 1.0, **kwargs)
+
+def Denormalization(**kwargs):
+    return Lambda(lambda x: (x + 1.0) * 127.5, **kwargs)
+
+def ConvBlock(x, filters=32, expansion=6, kernel=3):
+    x = Conv2D(filters * expansion, 1, padding='same')(x)
+    x = Activation('relu')(x)
+    x = Conv2D(int(filters * 0.8), 1, padding='same')(x)
+    x = Conv2D(filters, kernel, padding='same')(x)
+    return x
+
+def PaddingToIntegerMultiples(x, integer):
+    ph = (integer - K.shape(x)[1] % integer) % integer
+    pw = (integer - K.shape(x)[2] % integer) % integer
+    x = Lambda(tf.pad, arguments={'paddings':[[0, 0], [0, ph], [0, pw], [0, 0]]})(x)
+    return x
+
+def UpSamplingBlock(x, filters, scale):
+    x_y = ConvBlock(x, filters=filters*scale**2, expansion=6, kernel=3)
+    x_y = SubpixelConv2D(scale)(x_y)
+    return x_y
+
+def DownSamplingBlock(x, filters, scale):
+    x_y = DesubpixelConv2D(scale=scale)(x)
+    x_y = ConvBlock(x_y, filters=filters, expansion=6, kernel=3)
+    return x_y
+
+def SPENet(layers=8, joints=17):
+    inputs = Input(shape=(None, None, 3))
+    # normalize
+    x = Normalization()(inputs)
+    # padding
+    x = PaddingToIntegerMultiples(x, integer=16)
+    # conv2d
+    x = Conv2D(32, 5, padding='same')(x)
+    # x4
+    x4 = DownSamplingBlock(x, filters=32, scale=4)
+    # x8
+    x8 = DownSamplingBlock(x4, filters=64, scale=2)
+    # x16
+    x16 = DownSamplingBlock(x8, filters=128, scale=2)
+    for i in range(layers):
+        # x4
+        x4_x4 = ConvBlock(x4, filters=32, expansion=6, kernel=3)
+        # upsampling x8
+        x8_x4 = UpSamplingBlock(x8, filters=32, scale=2)
+        # upsampling x16
+        x16_x4 = UpSamplingBlock(x16, filters=32, scale=4)
+        # x8
+        x8_x8 = ConvBlock(x8, filters=64, expansion=6, kernel=3)
+        # upsampling x16
+        x16_x8 = UpSamplingBlock(x16, filters=64, scale=2)
+        # downsampling x4
+        x4_x8 = DownSamplingBlock(x4, filters=64, scale=2)
+        # x16
+        x16_x16 = ConvBlock(x16, filters=128, expansion=6, kernel=3)
+        # downsampling x4
+        x4_x16 = DownSamplingBlock(x4, filters=128, scale=4)
+        # downsampling x8
+        x8_x16 = DownSamplingBlock(x8, filters=128, scale=2)
+        # add
+        x4 = Add()([x4, x4_x4, x8_x4, x16_x4])
+        x8 = Add()([x8, x4_x8, x8_x8, x16_x8])
+        x16 = Add()([x16, x4_x16, x8_x16, x16_x16])
+        pass
+    # x4
+    x4_x4 = ConvBlock(x4, filters=32, expansion=6, kernel=3)
+    # upsampling x8
+    x8_x4 = UpSamplingBlock(x8, filters=32, scale=2)
+    # upsampling x16
+    x16_x4 = UpSamplingBlock(x16, filters=32, scale=4)
+    x4 = Add()([x4, x4_x4, x8_x4, x16_x4])
+    # x2 upsampling
+    x2 = ConvBlock(x4, filters=joints*2**2, expansion=6, kernel=3)
+    x2 = SubpixelConv2D(2)(x2)
+    # heatmaps
+    xh = K.shape(inputs)[1] // 2
+    xw = K.shape(inputs)[2] // 2
+    outputs = Lambda(lambda x: x[:, 0:xh, 0:xw, :])(x2)
+    return Model(inputs, outputs)
+
+'''TCPNet'''
+
+def TCPNet(step=5, features=51, ndim=7, units=256):
+    X = Input(shape=(step, features))
+    lstm_layer = LSTM(units, return_state=True, kernel_regularizer=keras.regularizers.l2(0.01)) 
+    dense_layer = Dense(ndim, activation='softmax')
+    a0 = Input(shape=(units, ), name='a0')
+    c0 = Input(shape=(units, ), name='c0')
+    a = a0
+    c = c0
+    for t in range(step):
+        x = Lambda(lambda x: X[:, t, :])(X)
+        x = Reshape((1, features))(x)
+        a, _, c = lstm_layer(x, initial_state=[a, c])
+        pass
+    Y = dense_layer(a)
+    model = Model([X, a0, c0], Y)
+    return model
+    
